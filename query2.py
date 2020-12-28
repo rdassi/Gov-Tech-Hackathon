1273826a6172e11d40b89a8e826da9c5e6c1d875
neelbhandari6 <30409992+neelbhandari6@users.noreply.github.com>
2020-12-28 13:07:37 +0530

    Query 2

---
+++ query2.py
@@ -0,0 +1,18 @@
+import pymongo
+import csv
+import json 
+
+from pymongo import MongoClient 
+import pandas as pd
+cluster=MongoClient("mongodb+srv://neelb:Fertilizer123@fertilizer.244tu.mongodb.net/<dbname>?retryWrites=true&w=majority")
+db=cluster["FertilizerData"]
+collection=db["IndiaSelect"]
+df= pd.read_csv('/Users/neelbhandari/Desktop/GovTech/FertilizerPrediction.csv',encoding="utf-8")
+
+def csv_to_json(filename, header=None):
+    data = pd.read_csv(filename, header=header)
+    return data.to_dict('records')
+collection.insert_many(csv_to_json('/Users/neelbhandari/Desktop/GovTech/indiaselected.csv', header=0))
+# for i in range(rows):
+#     post= {"temperature": df['Temparature'][i], "humidity":df['Humidity '][i], "moisture":df['Moisture'][i],"soil":df['Soil Type'][i],"crop_type":df['Nitrogen'][i],"potassium":df['Potassium'][i],"phosphrous":df['Phosphorous'][i],"fertilizer":df['Fertilizer Name'][i]}
+#     collection.insert_one(post)
